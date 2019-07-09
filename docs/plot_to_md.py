cc610afc4e066447bf4d4ecd858c8fab120f8b68
Teseo Schneider <teseo@bluewin.ch>
2019-07-09 19:20:23 -0400

    bug is not there

--- docs/plot_to_md.py
+++ docs/plot_to_md.py
@@ -1,8 +1,18 @@
 import meshplot
 import json
 
+total = ""
+first = True
 
 def mp_to_md(self):
-    return self.to_html()
+    global total
+    global first
+    res = self.to_html()
+    first = False
+
+    total += "aaaaa\n"+res
+    with open('export.html', 'w') as fp:
+            fp.write(total)
+    return res
 
 get_ipython().display_formatter.formatters["text/html"].for_type(meshplot.Viewer, mp_to_md)
